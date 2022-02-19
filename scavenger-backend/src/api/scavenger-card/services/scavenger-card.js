9ab9c751bbd2ba0da62e6a3dbb5aae7a6db11e0f
owen-duncan-snobel <oadster@cse.yorku.ca>
2022-02-18 22:08:42 -0500

    commit all with working backend

---
+++ scavenger-backend/src/api/scavenger-card/services/scavenger-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * scavenger-card service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::scavenger-card.scavenger-card');
